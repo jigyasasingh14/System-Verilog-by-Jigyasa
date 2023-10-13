25da76df4e64de82f749234deac8afec8036ed62
jigyasasingh14 <143309922+jigyasasingh14@users.noreply.github.com>
2023-10-13 19:41:35 +0530

    Update constraints_1.sv

--- Constraints/constraints_1.sv
+++ Constraints/constraints_1.sv
@@ -1,1 +1,26 @@
+//PATTERN-----5 -10 15 -20 25 -30
 
+class q1;
+
+  rand int a[];
+  constraint x {a.size==8;}
+  constraint y {foreach(a[i])
+    if (a%2==0)
+      a[i]==(i+1)*5;
+       else
+         a[i]==-(i+1)*5;
+               }  
+   
+  function void post_randomize;
+      $display("Randomized data is %p",a);
+    endfunction
+  
+endclass
+
+module top;
+  q1 q;
+  initial begin
+    q=new;
+    q.randomize;
+  end
+endmodule
